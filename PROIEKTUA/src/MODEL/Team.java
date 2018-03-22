3fa1897bb7f6b6690401c3eae1d5df5ece8621ff
DM3-1-16 <DM3-1-16@UH-012-11.H012053.NET>
2018-03-22 13:01:55 +0100

    Lehenengo aldaketak: -añadir la clase team -añadir los methods de team -crear el view y que funcionen los botones DELETE y EXIT

---
+++ PROIEKTUA/src/MODEL/Team.java
@@ -0,0 +1,66 @@
+package MODEL;
+
+import java.util.Scanner;
+import java.io.*;
+public class Team implements Serializable{
+    private String name;
+    private int numberofplayers;
+    private String dni; //Seria el dnii del entrenador(relacion)
+    
+public Team(){
+    setName();
+    setNumberofplayers();
+    setDni();
+}
+public void printInfo(){
+    getName(name);
+    getNumberofplayers(numberofplayers);
+    getDni(dni);
+}
+
+public void setName(){
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the name of the team: ");
+    name=in.nextLine();
+}
+public void setName(String newName){
+    name=newName;
+}
+public void getName(String newName){
+    System.out.println("Name of the team: "+name);
+}
+public String getName()
+    {
+        return name;
+    }
+
+public void setNumberofplayers(){
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the number of players: ");
+    numberofplayers=in.nextInt();
+}
+public void setNumberofplayers(int newNumberofplayers){
+    numberofplayers=newNumberofplayers;
+}
+public void getNumberofplayers(int newNumberofplayers){
+    System.out.println("Number of players: "+numberofplayers);
+}
+public int getNumberofplayers(){
+    return numberofplayers;
+}
+
+public void setDni(){
+    Scanner in = new Scanner(System.in);
+    System.out.println("Enter the dni of the coach: ");
+    dni=in.nextLine();
+}
+public void setDni(String newDni){
+    dni=newDni;
+}
+public void getDni(String newDni){
+    System.out.println("This is the dni of the coach: "+dni);
+}
+public String getDni(){
+    return dni;
+}
+}
---
+++ PROIEKTUA/src/MODEL/Team_methods.java
@@ -0,0 +1,334 @@
+package MODEL;
+
+import java.io.*;
+import java.util.ArrayList;
+
+public class Team_methods {
+    
+    public static void write_team() throws IOException
+    {
+       // definimos con una variable el file
+        File teamfile = new File("D:/team.txt");
+        
+        try
+        {
+            // creamos un objeto nuevo
+            Team te = new Team();
+            
+            // If the file doesn't exist, create a new ObjectOutputStream to write the header
+            if(!teamfile.exists())
+            {
+                FileOutputStream write1 = new FileOutputStream(teamfile);
+                ObjectOutputStream writeFile1 = new ObjectOutputStream(write1);
+                writeFile1.close();
+            }
+
+            // Create another ObjectOutputStream without the header to be able to write objects without overwriting
+            FileOutputStream write = new FileOutputStream(teamfile, true);
+            ObjectOutputStream writefile = new ObjectOutputStream(write)
+            {
+                @Override
+                protected void writeStreamHeader() throws IOException
+                {
+                    reset();
+                }
+            };
+            
+            // escribe el objeto y cierra los ficheros
+            writefile.writeObject(te);
+            write.close();
+            writefile.close();
+        }
+        catch(FileNotFoundException ex)
+        {
+            System.out.println("File not found.");
+        }
+    }
+    public static void show_team() throws IOException 
+    {
+         // definimos con una variable el file
+        File teamfile = new File("D:/team.txt");
+        
+        try
+        {
+            FileInputStream read = new FileInputStream(teamfile);
+            ObjectInputStream readFile = new ObjectInputStream(read);
+
+            try
+            {
+                while(true)
+                {
+                    Team te = (Team) readFile.readObject();
+                    te.printInfo();
+                    System.out.println();
+                }
+            }
+            catch(EOFException eof)
+            {
+                read.close();
+                readFile.close();
+            }
+        }
+        catch(ClassNotFoundException ex1)
+        {
+            System.out.println("Class not found.");
+        }
+        catch(FileNotFoundException ex)
+        {
+            System.out.println("File not found.");
+        }
+    }
+    public static void search_team() throws IOException, ClassNotFoundException 
+   {
+        // Definimos variables
+        String buscname = null, name;
+        boolean result = false;
+        File teamfile = new File("D:/team.txt");
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        try
+        {   
+            
+            
+            // Preguntamos por el dni al usuario
+            System.out.print("Enter the name of the team: ");
+            buscname = reader.readLine().toLowerCase();
+            
+            
+             
+            // Define a new ObjectInputStream
+            FileInputStream fs = new FileInputStream(teamfile);
+            ObjectInputStream os = new ObjectInputStream(fs);
+
+            try
+            {
+                while(true)
+                {
+                    Team te = (Team) os.readObject();
+                    name = te.getName().toLowerCase();
+                    
+                    // vemos haber si hay algun player con el dni que el usuario a metido
+                    if(name.contains(buscname))
+                    {
+                        System.out.println();
+                        te.printInfo();//imprime toda la informacion de eseplayer
+                        result = true;//para que salga del while
+                    }
+                }
+            }
+            catch(EOFException eof)
+            {
+                fs.close();
+                os.close();
+            }
+            
+            if(result == false)
+            {
+                System.out.println("The team that you want to search doesnt exist");
+            }
+        }
+       
+        catch(FileNotFoundException ex)
+        {
+            System.out.println("File not found.");
+        }
+    }
+    public static void delete_team() throws IOException, ClassNotFoundException
+    {
+         // Definimos las variables
+        String name, buscname = null;
+        File teamfile = new File("D:/team.txt");
+        File teamfile2 = new File("D:/teamcopy.txt");
+        
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        
+        try
+        {
+            // Define a new ObjectInputStream and a new ObjectOutputStream
+            FileInputStream fs = new FileInputStream(teamfile);
+            ObjectInputStream os = new ObjectInputStream(fs);
+            
+            // Create another ObjectOutputStream without the header to be able to write objects without overwriting
+            FileOutputStream fos = new FileOutputStream(teamfile2, true);
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+        
+            try
+            {
+                // Enseñamos todos los players que hay
+                //Preguntar al usuario por el dni
+                show_team();
+                System.out.print("Enter the name of the team you want to delete: ");
+                buscname = reader.readLine().toLowerCase();
+                
+                while(true)
+                {
+                    Team te = (Team) os.readObject();
+                    name = te.getName().toLowerCase();
+                    
+                    /* Check if there is a player with the dni the user has entered and show the result
+                    Si el dni que esta guardado NO es igual al que el usuario ha metido escribe en el segundo file
+                    el objeto entero (esto ira mirando todos los objetos que estan de antes)*/
+                    
+                    if(!name.equals(buscname))
+                    {
+                        oos.writeObject(te);
+                    }
+                }
+            }
+            catch(EOFException eof)
+            {
+                fs.close();
+                os.close();
+                fos.close();
+                oos.close();
+                teamfile.delete();
+                teamfile2.renameTo(teamfile); //renombra el segundo fitxero (el que no tiene ningun objeto repetido) llamandolo como el primero
+                System.out.println();
+                System.out.print("The team has been delete");
+                System.out.println(); //sino el menu queda pegado por eso ponemos una linea en blanco
+            }
+        }
+       
+        catch(FileNotFoundException ex)
+        {
+            System.out.println("File not found.");
+        }
+    }
+    /*
+    public static void add_Team ( ArrayList <Team> alteam)
+    {
+        Team te = new Team();
+        alteam.add(te);
+    }
+    
+    //Show all the teams
+    public static void show_Team(ArrayList <Team> alteam)
+    {
+       for (int i = 0; i < alteam.size(); i++)
+            {
+                Team te= alteam.get(i);
+                te.printInfo();
+            }
+    }
+    
+    public static void search_Team(ArrayList <Team> alteam) throws IOException 
+    {
+        String buscar,buscname;
+        boolean ikusi=true;//para que salga del for por si hay mas de un nombre.
+        
+        
+        BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
+        try
+        {
+            System.out.println("Enter the name of the team to check if it exists:");
+            buscar=reader.readLine();
+            
+            for (int i=0; i<alteam.size();i++)
+            {
+               buscname=alteam.get(i).getName();
+               
+               if (buscar.equals(buscname))
+               {
+                   
+                   Team te = alteam.get(i);
+                   te.printInfo();
+                   ikusi=false;
+               }  
+            }
+            if(ikusi==true)
+               {
+                System.out.println("Doesn´t exist the name that you enter.");
+               }
+            
+        }
+        catch(NumberFormatException error1)
+            {
+                System.out.println("The number you entered is not correct.");
+            }
+    }
+    
+    public static void delete_Team(ArrayList <Team> alteam) throws IOException
+    {
+        String nameteam,deleteteam;
+        boolean ikusi2 = true;
+        
+        BufferedReader reader = new BufferedReader (new InputStreamReader (System.in));
+        show_Team(alteam);
+        try
+        {
+            System.out.println("Put the name of the team that you want delete.");
+            nameteam=reader.readLine();
+            
+            for (int i=0; i<alteam.size();i++)
+            {
+               deleteteam=alteam.get(i).getName();
+               
+               if (deleteteam.equals(nameteam))
+               {
+                alteam.remove(i);
+                ikusi2=false;
+               }
+               
+            }
+            if (ikusi2==true)
+            {
+                System.out.println("The dni doesn´t exist.");
+            }
+            
+        }
+        
+        catch(NumberFormatException error1)
+            {
+                System.out.println("The number you entered is not correct.");
+            }
+    } */
+     public static void addTeam() throws FileNotFoundException{
+       try
+       {
+           Team te = new Team();
+           DataOutputStream save = new DataOutputStream(new FileOutputStream("team.dat",true));
+           save.writeUTF(te.getName());
+           save.writeInt(te.getNumberofplayers());
+           save.writeUTF(te.getDni());
+           save.close();
+           
+       }
+       catch (IOException er) 
+       {
+            System.out.println("ERROR");
+        }
+    }
+    
+    public static void erakutsiTeam(){
+        String name,dni;
+        int numberofplayers;
+        try
+        {
+        DataInputStream read = new DataInputStream(new FileInputStream("team.dat"));
+            try 
+            {
+                while (1==1)
+                {
+                    name = read.readUTF();
+                    numberofplayers = read.readInt();
+                    dni = read.readUTF();
+                    
+                    
+                    System.out.println("Name: "+name);
+                    System.out.println("Number ofplayers: "+numberofplayers);
+                    System.out.println("DNI: "+dni);
+                }
+            }
+        catch (EOFException er1) 
+        {
+            read.close();
+        }
+        }
+       catch (FileNotFoundException movida) {
+            System.out.println("File not found");
+        } catch (IOException fff) {
+            System.out.println("ERROR, PERO NO SÃ‰ CUÃ�L");
+        }
+    }
+    
+}
